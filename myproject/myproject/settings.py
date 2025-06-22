1259933303f6139af7d52277fb1268e657ea9086
kolesnikov19artem <kolesnikov19artem@yandex.ru>
2025-06-22 17:43:22 +0300

    fix

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -118,13 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # Добавьте в конец файла:
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myapp', 'static'),
-]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
